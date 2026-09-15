77d88f2e8f63659293d07df309384423a2208829
mnatsakanovaanisa <mnats.a@sch2009.net>
2026-09-15 13:01:37 +0300

    Add files via upload

---
+++ proc6.py
@@ -0,0 +1,10 @@
+def DigitCountSum(K):
+    K= syr(K)
+    count= 0
+    sum=0
+    for i in K:
+        count+=1
+        sum= sum= int(i)
+        return count, sum 
+    
+    print (DigitCountSum(12345))
