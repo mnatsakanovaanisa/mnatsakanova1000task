f640967bc6755ca4a1754911bc919f8a3ed58458
mnatsakanovaanisa <mnats.a@sch2009.net>
2026-09-20 13:00:42 +0300

    Add files via upload

--- for6.py
+++ for6.py
@@ -2,6 +2,7 @@ a=  float(input("Введите цену за 1 кг конфет: "))
 if a<0:
     print ("цена не может быть отрицательной")
 else:
-    for b in range(1, 11):
-        c= a*b
-    print (f"стомость {b} кг конфет : {c}")
+    for b in range(1, 6):
+         w= 1+ b*0.2
+    c= a*w
+    print (f"стомость {w} кг конфет : {c}")
